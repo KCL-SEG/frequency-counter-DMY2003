78f3efc648963a8b0ebc22b61e473e987ac7c190
Damian Mitrofan <damian.mitrofan@kcl.ac.uk>
2022-09-29 10:39:32 +0100

    Initial

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,11 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    # Your code goes here=]
+    for x in items:
+        x = str(x)
+        frequencies[x] = 0
+    for i in items:
+        i = str(i)
+        frequencies[i] = frequencies[i] + 1
     return frequencies
